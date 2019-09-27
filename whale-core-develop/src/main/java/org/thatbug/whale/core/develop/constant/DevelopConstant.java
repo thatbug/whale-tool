a8f206db7f267def4417f10522cd35ba24898575
thatbug <qzl>
2019-09-27 15:45:30 +0800

    10.代码生成器

---
+++ whale-core-develop/src/main/java/org/thatbug/whale/core/develop/constant/DevelopConstant.java
@@ -0,0 +1,20 @@
+package org.thatbug.whale.core.develop.constant;
+
+/**
+ * 系统常量
+ *
+ * @author qzl
+ * @date 11:14 2019/9/18
+ */
+public interface DevelopConstant {
+    /**
+     * sword 系统名
+     */
+    String SWORD_NAME = "sword";
+
+    /**
+     * saber 系统名
+     */
+    String SABER_NAME = "saber";
+}
+
